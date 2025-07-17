06167bc3012ae10004b0bf376b897f3c9898fe6e
urgence-permis <fodilmehdi49@gmail.com>
2025-07-18 00:29:55 +0200

    Feat: Configure custom favicons

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,27 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "The Mindset - Boostez votre esprit d'entrepreneur",
   description: "Rejoignez The Mindset, l'application qui aiguise votre pensée stratégique avec des capsules quotidiennes, des dilemmes et des conseils. Prenez de meilleures décisions et accélérez votre croissance.",
+  icons: {
+    icon: [
+      { url: '/favicon/favicon.ico', rel: 'icon', type: 'image/x-icon' },
+      { url: '/favicon/favicon-16x16.png', sizes: '16x16', type: 'image/png' },
+      { url: '/favicon/favicon-32x32.png', sizes: '32x32', type: 'image/png' },
+    ],
+    apple: [
+      { url: '/favicon/apple-touch-icon.png', type: 'image/png' },
+    ],
+    other: [
+      {
+        rel: 'android-chrome-192x192',
+        url: '/favicon/android-chrome-192x192.png',
+      },
+      {
+        rel: 'android-chrome-512x512',
+        url: '/favicon/android-chrome-512x512.png',
+      },
+    ],
+  },
+  manifest: '/favicon/site.webmanifest',
 };
 
 export default function RootLayout({
